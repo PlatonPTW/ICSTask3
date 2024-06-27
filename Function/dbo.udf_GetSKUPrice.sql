c1a8e7f29b6b8084c4860416fd436bb89eb63538
PlatonPTW <125853654+PlatonPTW@users.noreply.github.com>
2024-06-27 10:33:04 +0300

    Update dbo.udf_GetSKUPrice.sql

--- Function/dbo.udf_GetSKUPrice.sql
+++ Function/dbo.udf_GetSKUPrice.sql
@@ -1,13 +1,14 @@
 create function dbo.udf_GetSKUPrice (@ID_SKU INT)
-returns DECIMAL(18, 2)
+returns decimal(18, 2)
 as
 begin
-    declare @Price DECIMAL(18, 2);
+    declare @Price decimal(18, 2);
 
-    select @Price = SUM(Value) / SUM(Quantity)
+    select 
+        @Price = sum(Value) / sum(Quantity)
     from dbo.Basket
     where ID_SKU = @ID_SKU;
 
-    returns ISNULL(@Price, 0);
+    returns isnull(@Price, 0);
 end;
 GO
