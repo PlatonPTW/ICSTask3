b174a52fff4777c00b172167bf48c1b5379fb192
PlatonPTW <125853654+PlatonPTW@users.noreply.github.com>
2024-06-27 10:49:04 +0300

    Update dbo.TR_Basket_insert_update.sql

--- Trigger/dbo.TR_Basket_insert_update.sql
+++ Trigger/dbo.TR_Basket_insert_update.sql
@@ -1,19 +1,19 @@
-REATE TRIGGER dbo.TR_Basket_insert_update
-ON dbo.Basket
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    UPDATE b
-    SET DiscountValue = CASE 
-                            WHEN i.SKUCount >= 2 THEN b.Value * 0.05 
-                            ELSE 0 
-                        END
-    FROM dbo.Basket AS b
-    INNER JOIN inserted AS i ON b.ID = i.ID
-    INNER JOIN (
-        SELECT ID_SKU, COUNT(*) AS SKUCount
-        FROM inserted
-        GROUP BY ID_SKU
-    ) AS c ON b.ID_SKU = c.ID_SKU;
-END;
-GO
+reate trigger dbo.TR_Basket_insert_update
+on dbo.Basket
+after insert, update
+as
+begin
+    update b
+    set DiscountValue = case 
+                            when i.SKUCount >= 2 then b.Value * 0.05 
+                            else 0 
+                        end
+    from dbo.Basket as b
+    inner join inserted as i on b.ID = i.ID
+    inner join (
+        select ID_SKU, count(*) as SKUCount
+        from inserted
+        group by ID_SKU
+    ) as c on b.ID_SKU = c.ID_SKU;
+end;
+go
