a2d91b6afcb19860e19f6a6cc9c42e898e078a36
PlatonPTW <125853654+PlatonPTW@users.noreply.github.com>
2024-06-27 10:52:44 +0300

    Update CreateStructure.sql

--- Table/CreateStructure.sql
+++ Table/CreateStructure.sql
@@ -1,5 +1,5 @@
 create table dbo.SKU (
-    Id int identity(1,1) primary key,
+    ID int identity(1,1) primary key,
     Code as 's' + cast(ID as varchar(10)) unique,
     Name nvarchar(255) not null
 );
